b075ce98be215faeee5ead4c429c77261dd0d64e
Sanctuary1 <ryker.mineer@gmail.com>
2024-08-10 19:49:35 -0700

    Update LoginChat.cpp

--- src/LoginChat.cpp
+++ src/LoginChat.cpp
@@ -14,19 +14,26 @@ public:
     LoginChat() : PlayerScript("LoginChat") { }
 
     void OnLogin(Player* player) override {
-
-	std::string channelName = sConfigMgr->GetOption<std::string>("LoginChat.name", "world");
-        QueryResult result = CharacterDatabase.Query("SELECT channelId FROM channels WHERE name = '{}'", channelName.c_str());
-
-        if (!result) return;
-
-    	if (ChannelMgr* cMgr = ChannelMgr::forTeam(player->GetTeamId()))
-    	{
-        	if (Channel* channel = cMgr->GetJoinChannel("world", 9))
-            		channel->JoinChannel(player, "");
-    		cMgr->LoadChannels();
-	}
-
+	uint8 gender = player->getGender();
+    switch (gender)
+    {
+		if(player->getRace() == RACE_VRYKUL)
+		{
+		case GENDER_FEMALE:
+			if(player->!HasSpell(87841))
+			{
+				player->learnSpell(87841);
+			}
+		break;
+		case GENDER_MALE:
+			if(player->!HasSpell(87842))
+			{
+				player->learnSpell(87842);
+			}
+		break;
+		} 
+    }
+	
     }
 };
 
